761ff84e4bd6866dd35cbfba8336d3ad463f40a3
kristban <kristinbannasch@hotmail.com>
2024-12-21 20:27:07 +0000

    added search for city and temperature using API

--- src/index.js
+++ src/index.js
@@ -1,12 +1,30 @@
+function updateWeather(response) {
+  let temperatureElement = document.querySelector("#today-temperature");
+  let temperature = response.data.temperature.current;
+  let cityElement = document.querySelector("#city");
+
+  temperatureElement.innerHTML = Math.round(temperature);
+  cityElement.innerHTML = response.data.city;
+}
+
+function searchCity(city) {
+  let apiKey = "7a9df9a4f940btc116d43db796o3aa67";
+  let apiURL = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+
+  axios.get(apiURL).then(updateWeather);
+}
+
 function handleSearchSubmit(event) {
   event.preventDefault();
   let searchInput = document.querySelector("#search-field");
-  let cityElement = document.querySelector("#city");
-  cityElement.innerHTML = searchInput.value;
+
+  searchCity(searchInput.value);
 }
 
 let searchFieldElement = document.querySelector("#search-form");
 searchFieldElement.addEventListener("submit", handleSearchSubmit);
+
+searchCity("Cork")
 
 function formatDate(date) {
   let minutes = date.getMinutes();
