7dcee929becab26e1b2e5ba65704aa0f1ae4cb60
kristban <kristinbannasch@hotmail.com>
2024-12-21 22:16:23 +0000

    added humidity, wind, condition, time and icon

--- src/index.js
+++ src/index.js
@@ -2,9 +2,50 @@ function updateWeather(response) {
   let temperatureElement = document.querySelector("#today-temperature");
   let temperature = response.data.temperature.current;
   let cityElement = document.querySelector("#city");
+  let conditionElement = document.querySelector("#condition");
+  let humidityElement = document.querySelector("#humidity");
+  let windElement = document.querySelector("#wind");
+  let timeElement = document.querySelector("#time");
 
+  let date = new Date(response.data.time * 1000);
+
+  let iconElement = document.querySelector("#icon");
+
+  
   temperatureElement.innerHTML = Math.round(temperature);
   cityElement.innerHTML = response.data.city;
+  conditionElement.innerHTML = response.data.condition.description;
+  humidityElement.innerHTML = `${response.data.temperature.humidity} %`;
+  windElement.innerHTML = `${response.data.wind.speed} km/h`;
+  timeElement.innerHTML = formatDate(date);
+  iconElement.innerHTML = `<img src="${response.data.condition.icon_url}" class="weather-app-icon" />`;
+  
+}
+
+function formatDate(date) {
+  let hours = date.getHours();
+  let minutes = date.getMinutes();
+
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+
+  let day = days[date.getDay()];
+
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+  if (hours < 10) {
+    minutes = `0${hours}`;
+  }
+
+  return `${day} ${hours}:${minutes}`;
 }
 
 function searchCity(city) {
@@ -24,36 +65,5 @@ function handleSearchSubmit(event) {
 let searchFieldElement = document.querySelector("#search-form");
 searchFieldElement.addEventListener("submit", handleSearchSubmit);
 
-searchCity("Cork")
+searchCity("Cork");
 
-function formatDate(date) {
-  let minutes = date.getMinutes();
-  let hours = date.getHours();
-  let day = date.getDay();
-
-  let days = [
-    "Sunday",
-    "Monday",
-    "Tuesday",
-    "Wednesday",
-    "Thursday",
-    "Friday",
-    "Saturday",
-  ];
-
-  let formattedDay = days[day];
-
-  if (minutes < 10) {
-    minutes = `0${minutes}`;
-  }
-  if (hours < 10) {
-    minutes = `0${hours}`;
-  }
-
-  return `${formattedDay} ${hours}:${minutes}`;
-}
-
-let currentDateElement = document.querySelector("#currentDate");
-let currentDate = new Date();
-
-currentDateElement.innerHTML = formatDate(currentDate);
